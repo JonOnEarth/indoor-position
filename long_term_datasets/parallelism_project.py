d4f208d1726de451a9e6283dd9044e59362134e7
jononearth <mys123212@gmail.com>
2019-04-18 14:41:12 -0400

    version 1.0

--- long_term_datasets/parallelism_project.py
+++ long_term_datasets/parallelism_project.py
@@ -1,8 +1,7 @@
 import pandas as pd
 import numpy as np
 import tensorflow as tf
-from sklearn.preprocessing import scale
-from sklearn import preprocessing
+from keras.backend import tensorflow_backend as K
 import keras
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation
@@ -20,24 +19,21 @@ import argparse
 from sklearn.model_selection import KFold
 from statistics import mode, StatisticsError
 
-# from keras.layers import Conv1D, Dense, MaxPooling1D, Flatten, Input
 
-''' which need parallel:
-1. classification and regression;
-2. parameters: floor in regression;
-3. train parameters: [regularizers penalty]
-4. train parameters: [dropout]
-5. mini-batch graident decent
+''' 
+wifi indoor positioning project
+include floor classification and each floor regression
+Training on keras backend tensorflor-gpu
+
 '''
 
 # read data
 def read_data():
-    #path = 'C:/software/winpython-64bit-3.6.2.0qt5/notebooks/indoor position/long_term_datasets'
-    train_rss = pd.read_csv('train_rss.csv',header  = None)
-    train_coord = pd.read_csv('train_coords.csv', header  = None)
+    test_rss = pd.read_csv('train_rss.csv',header  = None)
+    test_coord = pd.read_csv('train_coords.csv', header  = None)
 
-    test_rss = pd.read_csv('test_rss.csv',header  = None)
-    test_coord = pd.read_csv('test_coords.csv',header  = None)
+    train_rss = pd.read_csv('test_rss.csv',header  = None)
+    train_coord = pd.read_csv('test_coords.csv',header  = None)
 
     return train_rss, train_coord, test_rss, test_coord
 
@@ -49,7 +45,7 @@ def data_classify():
     dummy_label = pd.get_dummies(train_labels)
     train_labels = np.asarray(dummy_label)
     test_labels = np.asarray(pd.get_dummies(test_labels))
-    return train_rss, train_labels, test_rss, test_labels
+    return train_rss.values, train_labels, test_rss.values, test_labels
 
 # read data for each floor positioning
 def data_regression():
@@ -71,8 +67,8 @@ def data_regression():
     print(train_coord.shape)
 
     test=test_ar[test_ar[:,-1]==3]
-    test_rss = test[0:5000, 0:448]
-    test_coord = test[0:5000, 448:450]
+    test_rss = test[:, 0:448]
+    test_coord = test[:, 448:450]
 
     return train_rss, train_coord, test_rss, test_coord
 
@@ -87,7 +83,7 @@ def predata(rss, locations):
     train_Xx = np.asarray(rss, dtype=np.float64)
     return train_Xx, train_Yy
 
-#split the train data into train and validation data
+#split the train data into train and validation data, this is simple version of validation, we also use cross validation
 def train_val(rss, locations):
     train_Xx, train_Yy = predata(rss, locations)
     train_x, val_x, train_y, val_y = train_test_split(train_Xx, train_Yy, test_size=0.25)
@@ -98,16 +94,12 @@ def accuracy(predictions, labels):
     error = np.sqrt(np.sum((predictions - labels)**2, 1))
     return error, np.mean(error)
 
-# def a function do cross validation
-def cv(rss):
-    kf = KFold(n_splits=5, random_state=2018)
-    return kf.split(rss)
 
 # def a function to do classify accuracy
 def class_accuracy(vote_labels, test_labels):
     count = 0
     for i in range(len(vote_labels)):
-        if (vote_labels[i]).all() == np.all(test_labels[i]):
+        if (vote_labels[i] == test_labels[i]).all():
             count = count + 1
     accuracy = 1.*count/len(vote_labels)
     return accuracy
@@ -122,7 +114,7 @@ def floor_accuracy(test_label, floor_est):
     return class_accuracy(vote_labels, test_label)
 
 # def a function to do positioning
-def model_regression(train_X, train_Y, val_X, val_Y,regularzation_penalty):
+def model_regression(train_X, train_Y, val_X, val_Y,regularzation_penalty, batch):
 
     # parameters
     num_input = train_X.shape[1]# input layer size
@@ -149,12 +141,12 @@ def model_regression(train_X, train_Y, val_X, val_Y,regularzation_penalty):
     earlyStopping=keras.callbacks.EarlyStopping(monitor='val_loss', patience=60, verbose=0, mode='auto')
     model.fit(train_X, train_Y,
               epochs=100,
-              batch_size=64,callbacks=[earlyStopping],validation_data=(val_X, val_Y))#tbCallBack
+              batch_size=batch,callbacks=[earlyStopping],validation_data=(val_X, val_Y))#tbCallBack
 
     return model
 
 # def a function to do classification
-def model_classification(train_X, val_X, train_Y, val_Y, regularzation_penalty):
+def model_classification(train_X, val_X, train_Y, val_Y, regularzation_penalty,batch):
 
     # parameters
     num_input = train_X.shape[1]# input layer size
@@ -179,7 +171,7 @@ def model_classification(train_X, val_X, train_Y, val_Y, regularzation_penalty):
     earlyStopping=keras.callbacks.EarlyStopping(monitor='val_loss', patience=60, verbose=0, mode='auto')
     model.fit(train_X, train_Y,
               epochs=100,
-              batch_size=64,callbacks=[earlyStopping],validation_data=(val_X, val_Y))#tbCallBack,
+              batch_size=batch,callbacks=[earlyStopping],validation_data=(val_X, val_Y))#tbCallBack,
 
     return model
 
@@ -188,30 +180,37 @@ if __name__ == "__main__":
     parser.add_argument('mode', help='Mode of operation',choices=['classification','positioning'])
     #parser.add_argument('output', help='Input file or list of files.')
     #parser.add_argument('input', help='File in which output is stored')
-    #parser.add_argument('--floor',default=3,type=float,help ="the floor will train")
+    parser.add_argument('--floor',default=3,type=float,help ="the floor will train")
+    parser.add_argument('--batch',default=64,type=int,help ="the batch size")
     parser.add_argument('--regularzation_penalty',default=0.03,type=float,help ="regularzation_penalty")
-    # parser.add_argument('--master',default="local[20]",help="Spark Master")
-    # parser.add_argument('--idfvalues',type=str,default="idf", help='File/directory containing IDF values. Used in TFIDF mode to compute TFIDF')
-    # parser.add_argument('--other',type=str,help = 'Score to which input score is to be compared. Used in SIM mode')
     args = parser.parse_args()
-    #sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
+
+    kf = KFold(n_splits=5, random_state=2018)
 
     if args.mode == "positioning":
         t1 = t.time()
 
         rss, locations, test_rss, test_locations = data_regression()
         test_X, test_Y = predata(test_rss, test_locations)
-        predict_Y_lst = np.zeros(len(test_X),5)
-        # get train_X, val_X, train_Y, val_Y
-        for i, (train_index, test_index) in enumerate(list(cv(rss))):
-            print("Fold", i)
+        predict_Y_lst = np.zeros((len(test_X),2,5))
 
+        # get train_X, val_X, train_Y, val_Y by cross validation
+        for i, (train_index, test_index) in enumerate(kf.split(rss)):
+            print("Fold", i)
+            t2 = t.time()
             train_X = rss[train_index]
             train_Y = locations[train_index]
-            val_X = test_rss[test_index]
-            val_Y = test_locations[test_index]
+            val_X = rss[test_index]
+            val_Y = locations[test_index]
 
-            model = model_regression(train_X, train_Y, val_X, val_Y, args.regularzation_penalty)
+            # with tf.Session(config=tf.ConfigProto(
+            #         intra_op_parallelism_threads=32,
+            #         inter_op_parallelism_threads=32,
+            #         allow_soft_placement=True,
+            #         log_device_placement=True)) as sess:
+            #     K.set_session(sess)
+
+            model = model_regression(train_X, train_Y, val_X, val_Y, args.regularzation_penalty, args.batch)
             # model evaluate
             train_loss = model.evaluate(train_X, train_Y, batch_size=len(train_Y))  # calculate the data in test mode(Keras)
             val_loss = model.evaluate(val_X, val_Y, batch_size=len(val_Y))
@@ -220,32 +219,37 @@ if __name__ == "__main__":
             print("Loss for validation data is", val_loss)
             print("Loss for test data is", test_loss)
             predict_Y = model.predict(test_X)
-            predict_Y_lst[:, i] = predict_Y
+            predict_Y_lst[:, :, i] = predict_Y
             error_t, accuracy_t = accuracy(predict_Y, test_Y)
+            print('\naccuracy_t:',accuracy_t)
+            print('\ntime for each fold:',t.time()-t2)
 
-        pre_Y_mean = np.mean(predict_Y_lst, axis=1)
+        pre_Y_mean = np.mean(predict_Y_lst, axis=2)
         error_t, accuracy_t = accuracy(pre_Y_mean, test_Y)
 
-        t2 = t.time()
-        print('accuracy_t',accuracy_t,'time', t2-t1)
-        #np.savetxt('args.output', accuracy_t , delimiter = ',')
+        t3 = t.time()
+        print('accuracy_t',accuracy_t,'time', t3-t1)
 
     if args.mode == "classification":
         t1 = t.time()
         # get train_X, val_X, train_Y, val_Y
         rss, floor, test_rss, test_floor = data_classify()
-        floor_est = np.zeros(len(test_rss), 5)
-        predict_Y_lst = np.zeros(len(test_rss), 5)
-        # get train_X, val_X, train_Y, val_Y
-        for i, (train_index, test_index) in enumerate(list(cv(rss))):
-            print("Fold", i)
+        floor_est = np.zeros((len(test_rss), 5))
+        predict_Y_lst = np.zeros((len(test_rss), 5))
 
+        # get train_X, val_X, train_Y, val_Y by cross validation
+        for i, (train_index, test_index) in enumerate(kf.split(rss)):
+            print("Fold", i)
+            t2 = t.time()
             train_X = rss[train_index]
             train_Y = floor[train_index]
-            val_X = test_rss[test_index]
-            val_Y = test_floor[test_index]
+            val_X = rss[test_index]
+            val_Y = floor[test_index]
 
-            model = model_classification(train_X, val_X, train_Y, val_Y,args.regularzation_penalty)
+            # with tf.Session(config=tf.ConfigProto(
+            #         intra_op_parallelism_threads=8)) as sess:
+            #     K.set_session(sess)
+            model = model_classification(train_X, val_X, train_Y, val_Y,args.regularzation_penalty,args.batch)
             # model evaluate
             train_loss, train_accuracy = model.evaluate(train_X, train_Y, batch_size=len(
                 train_Y))  # calculate the data in test mode(Keras)
@@ -255,11 +259,10 @@ if __name__ == "__main__":
             print("\n Loss for training data is %f, and the accuracy is %f \n", train_loss, train_accuracy)
             print("\n Loss for validation data is %f,and the accuracy is %f \n", val_loss, val_accuracy)
             print("\n Loss for test data is %f, and the accuracy is %f \n", test_loss, test_accuracy)
+            print('\ntime for each fold:',t.time()-t2)
 
         cv_test_accuracy = floor_accuracy(test_floor, floor_est)
 
-        t2 = t.time()
-        print('time',t2-t1, '\n cv_test_accuracy',cv_test_accuracy)
-        #np.savetxt('args.outpt' , test_accuracy, delimiter = ',')
-# positioning_accuracy_t.csv
-# classification_accuracy_t.csv
+        t3 = t.time()
+        print('time',t3-t1, '\n cv_test_accuracy',cv_test_accuracy)
+
